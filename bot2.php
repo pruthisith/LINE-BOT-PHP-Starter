3bba0e00a9385ef298cdb36a40b5af146f98a1a9
Terry <pruthisith@msn.com>
2017-03-12 13:22:22 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -9,6 +9,37 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
+		// Make a Fetch user profile
+		$uid = $event['source']['userId'];
+
+		$url = 'https://api.line.me/v2/bot/profile/'.$uid;
+		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, 0);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		$result = curl_exec($ch);
+		curl_close($ch);
+
+		$profileinfo = json_decode($result, true);
+
+
+		// Retrieve image picture
+		$urlprofile = $profileinfo['pictureUrl'].'/large';
+
+		$chpic = curl_init($urlprofile);
+		$saveuri = "/usr/share/nginx/html/profile/".$uid.".png";
+		$temppic = fopen($saveuri,"wb");
+		curl_setopt($chpic, CURLOPT_FILE, $temppic);
+		curl_setopt($chpic, CURLOPT_HEADER, 0);
+		curl_setopt($chpic, CURLOPT_FOLLOWLOCATION, true);
+		$resultpic = curl_exec($chpic);
+		curl_close($chpic);
+		fclose($saveuri);
+		
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
