3113d2beda6fc42f8e6f298fd519480e78469932
Terry <pruthisith@msn.com>
2017-03-12 13:32:47 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -50,7 +50,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'Hello '.$profileinfo['displayName'] 'You typed '.$text
+				'text' => 'Hello '.$profileinfo['displayName']. .'You typed '.$text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
